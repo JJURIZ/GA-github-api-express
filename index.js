ea748942d9710f760f12836f0e591022c202c9f4
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-18 15:20:45 -0500

    minor updates

--- index.js
+++ index.js
@@ -1,112 +1,106 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 //create the app
 const app = express();
-
 
 // Create a PORT
 const PORT = process.env.PORT || 3000;
 
 // THIS IS ME
-app.get('/me', (req, res) => {
+app.get("/me", (req, res) => {
+  // Access API endpoint from GitHub
+  axios.get("https://api.github.com/users/jjuriz").then((response) => {
+    //Print inside of terminal
+    const data = response.data;
+    // console.log(response.data);
+    const myData = {
+      login: data.login,
+      url: data.html_url,
+      name: data.name,
+    };
 
-    // Access API endpoint from GitHub
-    axios.get('https://api.github.com/users/jjuriz')
-    .then(response => {
-        //Print inside of terminal
-        const data = response.data;
-        // console.log(response.data);
-        const myData = {
-            login: data.login,
-            url: data.html_url,
-            name: data.name
-        };
-
-        console.log(myData);
-        res.send(myData);
-    })
-})
+    console.log(myData);
+    res.send(myData);
+  });
+});
 
 // Nitish route
-app.get('/nitish', (req, res) => {
-    axios.get('https://api.github.com/users/nitishdayal')
-    .then(response => {
-        const data = response.data;
-        const nitishObject = {
-            login: data.login,
-            url: data.html_url,
-            name: data.name
-        };
-        console.log(nitishObject);
-        res.send(nitishObject);
-    })
-})
+app.get("/nitish", (req, res) => {
+  axios.get("https://api.github.com/users/nitishdayal").then((response) => {
+    const data = response.data;
+    const nitishObject = {
+      login: data.login,
+      url: data.html_url,
+      name: data.name,
+    };
+    console.log(nitishObject);
+    res.send(nitishObject);
+  });
+});
 
 // Jenny Route
-app.get('/jenny', (req, res) => {
-    axios.get('https://api.github.com/users/ruvvet')
-    .then(response => {
-        const data = response.data;
-        const jennyObject = {
-            login: data.login,
-            url: data.html_url,
-            name: data.name
-        };
-        console.log(jennyObject);
-        res.send(jennyObject);
-    })
-})
+app.get("/jenny", (req, res) => {
+  axios.get("https://api.github.com/users/ruvvet").then((response) => {
+    const data = response.data;
+    const jennyObject = {
+      login: data.login,
+      url: data.html_url,
+      name: data.name,
+    };
+    console.log(jennyObject);
+    res.send(jennyObject);
+  });
+});
 
 // Simone Route
-app.get('/simone', (req, res) => {
-    axios.get('https://api.github.com/users/sschneeberg')
-    .then(response => {
-        const data = response.data;
-        const simoneObject = {
-            login: data.login,
-            url: data.html_url,
-            name: data.name
-        };
-        console.log(simoneObject);
-        res.send(simoneObject);
-    })
-})
+app.get("/simone", (req, res) => {
+  axios.get("https://api.github.com/users/sschneeberg").then((response) => {
+    const data = response.data;
+    const simoneObject = {
+      login: data.login,
+      url: data.html_url,
+      name: data.name,
+    };
+    console.log(simoneObject);
+    res.send(simoneObject);
+  });
+});
 
 // Alan Path
-app.get('/alan', (req, res) => {
-    axios.get('https://api.github.com/users/delayedaa')
-    .then(response => {
-        const data = response.data;
-        const alanObject = {
-            login: data.login,
-            url: data.html_url,
-            name: data.name
-        };
-        console.log(alanObject);
-        res.send(alanObject);
-    })
-})
+app.get("/alan", (req, res) => {
+  axios.get("https://api.github.com/users/delayedaa").then((response) => {
+    const data = response.data;
+    const alanObject = {
+      login: data.login,
+      url: data.html_url,
+      name: data.name,
+    };
+    console.log(alanObject);
+    res.send(alanObject);
+  });
+});
 
 // Corey Path
-app.get('/corey', (req, res) => {
-    axios.get('https://api.github.com/users/CoreyWilson319')
-    .then(response => {
-        const data = response.data;
-        const coreyObject = {
-            login: data.login,
-            url: data.html_url,
-            name: data.name
-        };
-        console.log(coreyObject);
-        res.send(coreyObject);
-    })
-})
-
+app.get("/corey", (req, res) => {
+  axios.get("https://api.github.com/users/CoreyWilson319").then((response) => {
+    const data = response.data;
+    const coreyObject = {
+      login: data.login,
+      url: data.html_url,
+      name: data.name,
+    };
+    console.log(coreyObject);
+    res.send(coreyObject);
+  });
+});
 
 // Listen on a PORT
 app.listen(PORT, () => {
-    console.log(`You're on PORT ${PORT} `)
-})
+  console.log(`You're on PORT ${PORT} `);
+});
 
 // login, html url, name
+
+
+
